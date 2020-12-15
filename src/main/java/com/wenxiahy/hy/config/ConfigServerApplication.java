52502ea96b2ede8dad8150d485419a68829d0ea1
周伟 <zhouwei@techfuser.net>
2020-12-15 13:00:18 +0800

    first commit

---
+++ src/main/java/com/wenxiahy/hy/config/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.wenxiahy.hy.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author zhouw
+ * @Description
+ * @Date 2020-12-14
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
